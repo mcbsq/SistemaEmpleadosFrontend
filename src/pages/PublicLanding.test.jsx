683176df9352e233c21c856cfc6045b6e546c5dc
mcbsq <mceballosq@redcibercom.com.mx>
2026-09-01 15:45:37 -0600

    test: cover public company onboarding

--- src/pages/PublicLanding.test.jsx
+++ src/pages/PublicLanding.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 import PublicLanding from "./PublicLanding";
 
 test("offers company registration and existing account access", () => {
   render(<MemoryRouter><PublicLanding /></MemoryRouter>);
   expect(screen.getByRole("heading", { name: /gestiona a tu equipo/i })).toBeInTheDocument();
-  expect(screen.getByRole("link", { name: /crear mi empresa/i })).toHaveAttribute("href", "/registro");
-  expect(screen.getByRole("link", { name: /ya tengo una cuenta/i })).toHaveAttribute("href", "/Login");
+  expect(screen.getAllByRole("link", { name: /crear mi empresa/i })[0]).toHaveAttribute("href", "/registro");
+  expect(screen.getAllByRole("link", { name: /ya tengo una cuenta/i })[0]).toHaveAttribute("href", "/Login");
 });
